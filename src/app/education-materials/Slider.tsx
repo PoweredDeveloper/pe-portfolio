2d12bdd717f5bc4a9e020545d0f013e624de3e00
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2024-11-29 15:29:02 +0300

    📌 Materials Update

--- src/app/education-materials/Slider.tsx
+++ src/app/education-materials/Slider.tsx
@@ -17,12 +17,14 @@ type Props = {
 
 export default function Slider({ files }: Props) {
   return (
-    <main className='pt-0 px-5 lg:pt-5 md:px-16 lg:px-32a grid gap-3'>
+    <main className='pt-0 px-5 lg:pt-5 md:px-16 lg:px-32a flex gap-3'>
       {files.map(((file: File, index: number) => (
-        <div key={index} className='flex flex-col gap-1 w-full lg:w-[300px] h-fit p-8 bg-zinc-300 border-[1px] rounded-xl border-zinc-500'>
-          <h1 className={`${font_unbounded.className} text-2xl flex gap-3 mb-2`}>{file.name}:</h1>
-          <span className='p-1 text-orange-500 bg-orange-300 rounded'>PPTX</span>
-          <Link href={`/materials/${file.file_path}`} className='text-lg text-accent font-semibold flex items-center gap-3' download={true}><LuDownload /> Скачать</Link>
+        <div key={index} className='flex flex-col justify-between gap-1 w-full lg:min-h-max lg:min-w-1/3 p-8 bg-[#fcfcfc] border-[1px] rounded-xl border-bleak'>
+          <h1 className={`${font_unbounded.className} text-2xl flex gap-3 mb-6`}>{file.name}:</h1>
+          <div className='flex flex-col gap-2'>
+            <span className='p-1 text-orange-400 bg-orange-200 rounded-lg font-semibold text-xs max-w-min px-2'>PPTX</span>
+            <Link href={`/materials/${file.file_path}`} className='text-lg text-accent font-semibold flex items-center gap-3' download={true}><LuDownload /> Скачать</Link>
+          </div>
         </div>
       )))}
     </main>
